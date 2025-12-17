a12c7128c112bcfcfeca2f7fd71767e5fa28abfe
SilverCaesar <juliusrosalin@gmail.com>
2025-12-17 15:09:11 -0600

    Update nav menu links and use DbContextFactory
    - Changed "Services" link in NavMenu to point to "/OldServices" for all users; added "/services" link for authorized users only.
    - Removed "My Profile" link from authorized user section.
    - Switched ApplicationDbContext registration to AddDbContextFactory in Program.cs for improved DbContext instantiation flexibility.

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
 builder.Services.AddQuickGridEntityFrameworkAdapter();
